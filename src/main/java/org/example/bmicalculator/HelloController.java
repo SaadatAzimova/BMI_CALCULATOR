936e34c5cf214e04233886edb0f5cef34810fff7
SaadatAzimova <saadatazimova5@gmail.com>
2024-10-12 21:23:50 +0600

    add menu bar

--- src/main/java/org/example/bmicalculator/HelloController.java
+++ src/main/java/org/example/bmicalculator/HelloController.java
@@ -1,7 +1,9 @@
 package org.example.bmicalculator;
 
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.collections.FXCollections;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -17,11 +19,12 @@ public class HelloController {
     @FXML
     private ChoiceBox<String> unitChoice;
     @FXML
+    //Add available unit options to the choice box
     public void initialize() {
         unitChoice.setItems(FXCollections.observableArrayList("Metric (kg/m)", "English (lb/in)"));
-        unitChoice.setValue("Metric (kg/m)");
     }
     @FXML
+    //Method for button click
     protected void onButtonClick() {
         double weightValue = Double.parseDouble(weight.getText());
         double heightValue = Double.parseDouble(height.getText());
@@ -30,10 +33,10 @@ public class HelloController {
         if ("Metric (kg/m)".equals(unit)) {
             bmi = weightValue / Math.pow(heightValue, 2);
         } else if ("English (lb/in)".equals(unit)) {
-            bmi = (weightValue * 703) / Math.pow(heightValue, 2);
+            bmi = (weightValue * 703) /*converting lbs to kg*/ / Math.pow(heightValue, 2);
         }
         String status ="";
-
+        //Checking bmi and setting status
         if(bmi<18.5) {
             status="Underweight";
         }
@@ -47,5 +50,31 @@ public class HelloController {
             status="Obese:";
         }
         result.setText(String.format("%s: %.2f", status, bmi));
+    }
+    //Method for Exit menu item
+    @FXML
+    protected void handleExit() {
+        Platform.exit();  //Exits the application
+    }
+
+    //Method for Clear menu item
+    @FXML
+    protected void handleClear() {
+        weight.clear();//Clears the weight text field
+        height.clear();// Clears the height text field
+        unitChoice.setValue("Metric (kg/m)");// Resets the choice box
+        result.setText("");// Clears the result label
+    }
+
+    //Method for About menu item
+    @FXML
+    protected void handleAbout() {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("About BMI Calculator");
+        alert.setHeaderText(null);// removes the header text from the Alert dialog
+        alert.setContentText("This is a simple BMI calculator. Enter your weight and height, "
+                + "choose the unit system (Metric or English), and press the 'Calculate' button "
+                + "to get your BMI and category (Underweight, Normal, Overweight, or Obese).");
+        alert.showAndWait();//displays the alert and waits for the user to respond
     }
 }
